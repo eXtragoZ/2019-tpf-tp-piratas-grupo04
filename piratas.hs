9858faa8a672e769ffb02729743b603156ac07cb
Lautaro <lautaro@lautaro.com>
2019-04-02 12:07:40 -0300

    now we know if a pirate is lucky

--- piratas.hs
+++ piratas.hs
@@ -10,22 +10,30 @@ data Tesoro = Tesoro {
 
 frascoArena = Tesoro "frasco de arena" 0
 
-piratas = [
-    Pirata "Jack Sparrow" [
-        Tesoro "Brujula que apunta a lo que mas deseas" 1000,
-        frascoArena
-        ],
-    Pirata "David Jones" [
-        Tesoro "Cajita musical" 1
-        ],
-    Pirata "Anne Bonny" [
-        Tesoro "Doblones" 100,
-        frascoArena
-        ]
-    ]  
+jackSparrow = Pirata "Jack Sparrow" [
+    frascoArena, 
+    Tesoro "Brujula que apunta a lo que mas deseas" 1000
+    ]
+
+davidJones = Pirata "David Jones" [
+    Tesoro "Cajita musical" 1
+    ]
+
+anneBonny = Pirata "Anne Bonny" [
+    Tesoro "Doblones" 100,
+    frascoArena
+    ]
+
+piratas = [jackSparrow,davidJones,anneBonny]  
 
 cantidadTesorosPirata :: Pirata -> Int
 cantidadTesorosPirata pirata = length (botin pirata)
 
---pirataEsAfortunado :: Pirata -> Bool
---pirataEsAfortunado pirata = sum valor botin pirata
+valoresTesorosPirata :: Pirata -> [Double]
+valoresTesorosPirata pirata = map valor (botin pirata)
+
+valorTotalTesoros :: Pirata -> Double
+valorTotalTesoros = sum.valoresTesorosPirata --pirata = sum (valoresTesorosPirata pirata)
+
+pirataEsAfortunado :: Pirata -> Bool
+pirataEsAfortunado = (>10000).valorTotalTesoros
